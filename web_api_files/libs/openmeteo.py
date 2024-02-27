68da734b86e52ceba5df4a48a268ddfd7b343912
Juan Manuel Castro Fuentes <142842308+JuanCastroFuentes@users.noreply.github.com>
2024-02-27 10:55:54 -0300

    Flask_and_SQL
    Conexión a database. Empiezo a hacer el script para flaskear la user request y devolver datos de la database.

--- web_api_files/libs/openmeteo.py
+++ web_api_files/libs/openmeteo.py
@@ -6,5 +6,5 @@ class MarineOpenMeteoClient():
     def __init__(self):
             pass
     def latest(self,latitude,longitude):
-        return requests.get(f"{self.BASE_URL}?latitude={latitude}&longitude={longitude}&daily=wave_height_max,wave_direction_dominant,wave_period_max,wind_wave_height_max,wind_wave_direction_dominant,wind_wave_period_max,swell_wave_height_max,swell_wave_direction_dominant,swell_wave_period_max&timezone=America%2FSao_Paulo&past_days=3&forecast_days=3")
+        return requests.get(f"{self.BASE_URL}?latitude={latitude}&longitude={longitude}&daily=wave_height_max,wave_direction_dominant,wave_period_max,wind_wave_height_max,wind_wave_direction_dominant,wind_wave_period_max,wind_wave_peak_period_max,swell_wave_height_max,swell_wave_direction_dominant,swell_wave_period_max,swell_wave_peak_period_max&timezone=America%2FSao_Paulo&past_days=3&forecast_days=3")
     
